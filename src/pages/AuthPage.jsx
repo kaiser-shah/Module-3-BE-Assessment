04c97349a97f2c4b6d32ad393354d0a30cae2185
kaiser-shah <shah.docklands@gmail.com>
2025-07-07 14:35:09 +0800

    edit to AuthPage import app.css

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { Button, Col, Image, Row, Modal, Form } from "react-bootstrap";
-import "../app.css";
+import "../App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import useLocalStorage from "use-local-storage";
